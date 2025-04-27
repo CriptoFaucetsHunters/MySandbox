e7ee07c1c3184efa78f244905f12bd119e8c9677
CriptoFaucetsHunters <criptofaucetshunters@gmail.com>
2025-04-26 22:11:31 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Función para alternar el menú
 function toggleMenu() {
-  const menu = document.getElementById('menu');
+  const menu = document.querySelector('.menu'); // Usa .menu en lugar de #menu
   const menuIcon = document.querySelector('.menu-icon');
   menu.classList.toggle('active');
   menuIcon.classList.toggle('active'); // Agrega/quita la clase 'active'
@@ -8,11 +8,13 @@ function toggleMenu() {
 
 // Cerrar el menú al hacer clic fuera de él
 document.addEventListener('click', function(event) {
-  const menu = document.getElementById('menu');
+  const menu = document.querySelector('.menu'); // Usa .menu en lugar de #menu
   const menuIcon = document.querySelector('.menu-icon');
 
-  // Si el clic no es en el ícono ni en el menú, cierra el menú
-  if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+  // Si el menú está activo y el clic no es en el ícono ni en el menú, cierra el menú
+  if (menu.classList.contains('active') && 
+      !menu.contains(event.target) && 
+      !menuIcon.contains(event.target)) {
     menu.classList.remove('active');
     menuIcon.classList.remove('active');
   }
