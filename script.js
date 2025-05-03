43fe474f76549a692ee2270f3afb8abeb873d4e2
CriptoFaucetsHunters <criptofaucetshunters@gmail.com>
2025-05-03 11:54:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // script.js
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el header:', error));
 
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar el footer:', error));
+function loadHTMLFile(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            // Forzamos un redibujado para que el sticky funcione bien
+            window.getComputedStyle(document.body).display;
+        })
+        .catch(error => console.error('Error al cargar:', error));
+}
+
+loadHTMLFile('header-placeholder', 'header.html');
+loadHTMLFile('footer-placeholder', 'footer.html');
